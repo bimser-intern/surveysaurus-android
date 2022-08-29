a64c759ac7891f315577df18c126e3de3c58ff4c
Keremerk <kereme@windowslive.com>
2022-08-29 16:41:09 +0300

    [bugfix]-[42]-[semver:minor]-Sayfalarda bulunan tasarım hatalarının bazıları giderildi.

--- app/src/main/java/com/android/surveysaurus/fragment/FillSurveyFragment.kt
+++ app/src/main/java/com/android/surveysaurus/fragment/FillSurveyFragment.kt
@@ -5,19 +5,15 @@ import android.content.Context
 import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
-import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
-
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.constraintlayout.widget.VirtualLayout
 import androidx.core.content.res.ResourcesCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.Navigation
@@ -35,6 +31,7 @@ class FillSurveyFragment : Fragment() {
     private val binding get() = _binding!!
     private var index: Int? = null
     private var percentence: Int? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -63,6 +60,7 @@ class FillSurveyFragment : Fragment() {
 
             binding.addQuestionFill.text = survey.question
             binding.addTitleFill.text = survey.title
+
 
             for (item in 0 until survey.choices.size) {
 
@@ -295,8 +293,6 @@ class FillSurveyFragment : Fragment() {
                                                       binding.root.addView(yuzde)
 
                                                   }*/
-
-
 
                                                 println("succes")
                                             } else {
